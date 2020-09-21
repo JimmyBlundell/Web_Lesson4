cd80912cb686dd58fb141ec0ecd644d3b1e35515
Jimmy Blundell <jblundell123@gmail.com>
2020-09-21 02:19:27 -0500

    Initial commit

---
+++ ICP/Hover/hover.js
@@ -0,0 +1,35 @@
+function upDate(previewPic) {
+    /* In this function you should
+       1) change the url for the background image of the div with the id = "image"
+       to the source file of the preview image
+
+       2) Change the text  of the div with the id = "image"
+       to the alt text of the preview image
+       */
+    $( ".preview" ).hover(
+        function() {
+            $("#image").css({"background-image": `url(${this.src})`});
+            $("#image").text(this.alt);
+        }
+    );
+};
+
+
+
+function unDo() {
+    /* In this function you should
+   1) Update the url for the background image of the div with the id = "image"
+   back to the orginal-image.  You can use the css code to see what that original URL was
+
+   2) Change the text  of the div with the id = "image"
+   back to the original text.  You can use the html code to see what that original text was
+   */
+
+    $( ".preview" ).hover(
+        function() {
+            $("#image").css({"background-image": ""});
+            $("#image").text("Hover over an image below to display here");
+        }
+    );
+
+}
