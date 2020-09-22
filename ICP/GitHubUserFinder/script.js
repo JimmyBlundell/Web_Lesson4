7c91b43de76841a17904d501fe9f4712f0252532
Jimmy Blundell <jblundell123@gmail.com>
2020-09-22 00:05:02 -0500

    Added requirements for second part of assignment

--- ICP/GitHubUserFinder/script.js
+++ ICP/GitHubUserFinder/script.js
@@ -1,14 +1,33 @@
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it.
     // The function should finally return the object(it now contains the response!)
+    fetch("https://api.github.com/users/" + user)
+        .then((result) => {
+            if(result.status === 200) {
+                let valid_user = result.json()
+                valid_user.then((user_info) => showUser(user_info))
+            } else {
+                noSuchUser(user)
+            }
+        })
 }
 
-function showUser(user) {
+function showUser(user_info) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    $("#user_name").text("User name: " + user_info.name)
+    $("#profile_pic").html("<img src='" + user_info.avatar_url + "' width='90%'>")
+    $("#user_id").text("User ID: " + user_info.id)
+    $("#user_url").html("<a href='"+ user_info.html_url + "/' > Github Page </a>")
+
 }
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
+    $("#user_name").text("User not found!")
+    $("#profile_pic").html("<img src='' width='90%'>")
+    $("#user_id").text("User ID not found!")
+    $("#user_url").html("<a href='' > GitHub Page not found </a>")
+
 }
 
 $(document).ready(function () {
